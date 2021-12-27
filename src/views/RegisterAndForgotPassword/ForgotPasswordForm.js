e406fc3483df27af83fd188646990b8a4ff93b7a
Kağan Hamzaçebi <kagan.hamzacebi@ortus.com.tr>
2021-12-27 14:25:09 -0800

    Service directory moved

--- src/views/RegisterAndForgotPassword/ForgotPasswordForm.js
+++ src/views/RegisterAndForgotPassword/ForgotPasswordForm.js
@@ -1,5 +1,5 @@
 import {useForm} from "react-hook-form";
-import UserService from "../service/UserService";
+import UserService from "../../service/UserService";
 
 export default function ForgotPasswordForm() {
 
