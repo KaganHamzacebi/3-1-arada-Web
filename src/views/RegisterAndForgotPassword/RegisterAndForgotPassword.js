0fbb5e80de633f5ab0e12a875aabdf07c32b6b0f
Kağan Hamzaçebi <kagan.hamzacebi@ortus.com.tr>
2022-01-27 06:46:24 +0300

    register ekran guncellemesi, assetler eklendi

--- src/views/RegisterAndForgotPassword/RegisterAndForgotPassword.js
+++ src/views/RegisterAndForgotPassword/RegisterAndForgotPassword.js
@@ -3,6 +3,8 @@ import {LocationMarkerIcon} from "@heroicons/react/outline";
 import RegisterForm from "./RegisterForm";
 import {useState} from "react";
 import ForgotPasswordForm from "./ForgotPasswordForm";
+import LogoWhite from "../../assets/images/logo_white.png";
+import LogoGreen from "../../assets/images/logo_green.png";
 
 export default function RegisterAndForgotPassword() {
 
@@ -10,14 +12,15 @@ export default function RegisterAndForgotPassword() {
 
     return (
         <div className="m-auto">
-            <div className="flex flex-end">
-                <div className="p-12 bg-theme-green rounded-l-lg shadow-xl">
+            <div className="grid grid-cols-1 p-6 lg:flex lg:p-0">
+                <img src={LogoGreen} alt="logo_mobile" className="w-32 h-32 block lg:hidden m-auto"/>
+                <div className="p-12 hidden lg:block bg-theme-green rounded-l-lg shadow-xl">
                     <div className="h-full w-full py-6 grid grid-rows-2">
                         <div id="Logo" className="flex flex-col">
-                            <AcademicCapIcon className="w-32 h-32 text-white m-auto"/>
+                            <img src={LogoWhite} alt="logo" className="w-36 h-36 hidden lg:block m-auto"/>
                             <h1 className="text-4xl font-bold text-white text-center">3in1</h1>
                         </div>
-                        <div className="flex self-end">
+                        <div className="flex">
                             <ol className="text-white text-sm m-auto space-y-2">
                                 <li className="flex"><LocationMarkerIcon className="w-4 h-4"/>
                                     <span className="ml-2">Simply create your calendar</span>
